b280d27f214242d97f509f07485c63426b409103
Tom Gielen <tomgielen007@gmail.com>
2018-10-13 18:28:13 +0200

    Message user object fixed

--- api/models/Message.js
+++ api/models/Message.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const Message = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    user: [{ _id: Number, name: String }],
+    user: { _id: Number, name: String },
     text: { type: String, require: true, },
     createdAt: { type: Date, require: true, },
     relation_id: { type: Number, require: true, },
