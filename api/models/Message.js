0bf74f3d4cb493e163d19f086287b170b29286cf
rickkorsten <rickkorsten@live.nl>
2018-10-18 12:20:02 +0200

    fix

--- api/models/Message.js
+++ api/models/Message.js
@@ -2,9 +2,12 @@ const mongoose = require('mongoose')
 
 const Message = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    user: { _id: String, name: String },
+    user: { 
+        _id: String, 
+        name: String 
+    },
     text: { type: String, require: true, },
     createdAt: { type: Date, require: true, },
-    relation_id: { type: Number, require: true, },
+    relation_id: mongoose.Schema.Types.ObjectId,
 })
 module.exports = mongoose.model('Message', Message)
